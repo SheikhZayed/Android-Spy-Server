b59e7c1b6e9cd7b12c2465c7620a18283bd4fd95
Ashif Ismail <cyber2maro@gmail.com>
2017-08-09 16:14:17 +0530

    added simple get response for /

--- src/main/java/io/github/ashif/androidspyserver/AndroidspyServerApplication.java
+++ src/main/java/io/github/ashif/androidspyserver/AndroidspyServerApplication.java
@@ -2,11 +2,19 @@ package io.github.ashif.androidspyserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class AndroidspyServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AndroidspyServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AndroidspyServerApplication.class, args);
+    }
+
+    @GetMapping(path = "/")
+    public String sayHello() {
+        return "Hay,How are you ?";
+    }
 }
