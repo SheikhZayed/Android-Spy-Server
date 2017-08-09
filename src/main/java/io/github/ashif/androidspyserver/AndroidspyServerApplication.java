5d39284e8638c8d2071cc28a0264da05e346b6fb
abdul.ashif <abdul.ashif@techjini.com>
2017-08-09 14:53:39 +0530

    project started

---
+++ src/main/java/io/github/ashif/androidspyserver/AndroidspyServerApplication.java
@@ -0,0 +1,12 @@
+package io.github.ashif.androidspyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndroidspyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndroidspyServerApplication.class, args);
+	}
+}
