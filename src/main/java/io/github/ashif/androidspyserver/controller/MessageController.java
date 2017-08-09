7d8e39269f9678cae6ab3f888b4d9fb643dcf708
Ashif Ismail <cyber2maro@gmail.com>
2017-08-09 16:42:08 +0530

    added logger support

--- src/main/java/io/github/ashif/androidspyserver/controller/MessageController.java
+++ src/main/java/io/github/ashif/androidspyserver/controller/MessageController.java
@@ -2,6 +2,8 @@ package io.github.ashif.androidspyserver.controller;
 
 import io.github.ashif.androidspyserver.data.model.Greeting;
 import io.github.ashif.androidspyserver.data.model.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -14,9 +16,12 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MessageController {
 
+    private static final Logger logger = LoggerFactory.getLogger(MessageController.class);
+
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Greeting sendResponse(Message incomingMessage) {
+        logger.debug(incomingMessage.getMessage());
         return new Greeting("Hello " + incomingMessage.getMessage());
     }
 }
